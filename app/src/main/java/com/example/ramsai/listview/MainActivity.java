3c850dc6be3319a1fba8f495be74a5c4ab5a1876
ramsai <ramsaijanapana@yahoo.in>
2017-02-21 23:02:38 -0600

    list view demo application
    a side project based on List VIew

---
+++ app/src/main/java/com/example/ramsai/listview/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.ramsai.listview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ListView listView = (ListView)findViewById(R.id.listView);
+        final ArrayList<String> friends = new ArrayList<String>(asList("Ramesh","Suresh","Ganesh","Naresh","Nagesh"));
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this,android.R.layout.simple_list_item_1,friends);
+        listView.setAdapter(arrayAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(getApplicationContext(),"Hello "+ friends.get(position), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
